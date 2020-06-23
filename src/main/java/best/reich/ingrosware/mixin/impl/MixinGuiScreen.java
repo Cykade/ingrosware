a9b4dfebe98e85ff8e8dab8d2eb6d16b282195ee
TBM <thegalaxygamesyt@gmail.com>
2020-06-23 16:42:36 +0100

    Fixed Mixin For DrawDefaultBackgroundEvent

--- src/main/java/best/reich/ingrosware/mixin/impl/MixinGuiScreen.java
+++ src/main/java/best/reich/ingrosware/mixin/impl/MixinGuiScreen.java
@@ -1,12 +1,12 @@
 package best.reich.ingrosware.mixin.impl;
 
 import best.reich.ingrosware.IngrosWare;
+import best.reich.ingrosware.event.impl.render.DrawDefaultBackgroundEvent;
 import net.minecraft.client.gui.GuiScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import us.devs.ingrosware.event.impl.render.DrawDefaultBackgroundEvent;
 
 @Mixin(GuiScreen.class)
 public class MixinGuiScreen {
